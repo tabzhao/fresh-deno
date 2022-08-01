03b3c893dc0f243e1af23ca24410d48267b5037a
tab <tab@jishu6.com>
2022-08-01 16:48:48 +0800

    添加测试代码

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,6 +1,15 @@
 /** @jsx h */
 import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
+import { PageProps, Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  GET(_, ctx) {
+    console.log(ctx.params.name)
+    return new Response(JSON.stringify("hello world,"+ ctx.params.name+ " !"), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+}
 
 export default function Greet(props: PageProps) {
   return <div>Hello {props.params.name}</div>;
